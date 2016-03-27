9995d49e371e6f1cbe42d6b1eb6aadd364eb97d0
Rakapudl <rakapudl@gmail.com>
2016-03-27 15:28:50 +0300

    v.0.0.3

---
+++ newProj/js/app.js
@@ -0,0 +1,34 @@
+$(function(){
+
+	$('.slider1').mobilyslider();
+
+	$('.slider2').mobilyslider({
+		transition: 'horizontal',
+		animationSpeed: 500,
+		autoplay: false,
+		autoplaySpeed: 3000,
+		pauseOnHover: true,
+		bullets: false
+	});
+
+	$('.slider3').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 500,
+      	autoplay: true,
+		autoplaySpeed: 10000,
+		bullets: true,
+      	pauseOnHover: true,
+		arrowsHide: false
+	});
+
+	$('.slider4').mobilyslider({
+		transition: 'fade',
+		animationSpeed: 500,
+      	autoplay: false,
+		autoplaySpeed: 60000,
+		bullets: false,
+      	pauseOnHover: true,
+		arrowsHide: false
+	});
+
+});
